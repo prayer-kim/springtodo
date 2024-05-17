aff305ab16e36e028232a3b7622b0ca3e978cd58
prayer-kim <prayer.kim@gmail.com>
2024-05-17 15:56:39 +0900

    spring boot aop 설정 추가

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ spring-boot framework 교육을 통해 초기 설정
 - 로그 수집
   - splunk (유료)
   - ELK (무료)
+
+- AOP 설정
+  - maven repository에서 spring boot aop 검색(https://mvnrepository.com/)
+    - Spring Boot Starter AOP
--- src/main/java/kr/re/kitri/hellogram/HellogramApplication.java
+++ src/main/java/kr/re/kitri/hellogram/HellogramApplication.java
@@ -2,8 +2,10 @@ package kr.re.kitri.hellogram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class HellogramApplication {
 
 	public static void main(String[] args) {
